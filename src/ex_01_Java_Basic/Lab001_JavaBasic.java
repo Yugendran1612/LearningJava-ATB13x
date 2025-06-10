b0ecb590761a0617b73ba4aeb89157f0aef71aa5
Yugendran1612 <yugendran.qa@gmail.com>
2025-06-10 20:34:22 +0530

    trying to push code lab001 and Lab002_Java

---
+++ src/ex_01_Java_Basic/Lab001_JavaBasic.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab001_JavaBasic {
+
+    public static void main(String[] arg) {
+
+    System.out.println("Hello");
+    System.out.println("Pushing to GitHub_001");
+    }
+}
