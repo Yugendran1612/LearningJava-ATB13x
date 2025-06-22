71e28f11be7be8f6853678a1a398c7610b036fd7
Yugendran1612 <yugendran.qa@gmail.com>
2025-06-22 09:58:04 +0530

    Class name renamed

---
+++ src/Excersice/ex_02_Arithmetic_Operator.java
@@ -0,0 +1,18 @@
+package Excersice;
+
+public class ex_02_Arithmetic_Operator {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        int add = a+b;
+        System.out.println("The addition value is "+ add);
+        int sub = a-b;
+        System.out.println("The substraction value is "+ sub);
+        int mul = a*b;
+        System.out.println("The multiplication value is "+ mul);
+        int div = a/b;
+        System.out.println("The division value is "+ div);
+        int mod = a%b;
+        System.out.println("The modules value is "+ mod);
+    }
+}
